c52e40e9ed2f322dbbbb61450f52215afa1b628f
Dimitrios Desyllas (pc_magas) <you@example.com>
2017-04-07 17:54:35 +0300

    Filter By MMSI

--- dimitriosDesyllas/src/AppBundle/Controller/VeselApiEndpointsController.php
+++ dimitriosDesyllas/src/AppBundle/Controller/VeselApiEndpointsController.php
@@ -15,7 +15,7 @@ class VeselApiEndpointsController extends Controller
 	const RESPONSE_XML='application/xml';
 	const RESPONSE_CSV='text/csv';
 	const RESPONSE_JSON='application/json';
-	
+
 	/**
 	 * Fetch all ship routes as json
 	 * @Route("/routes.json",name="getRoutesAsJson")
@@ -35,84 +35,84 @@ class VeselApiEndpointsController extends Controller
 	{
 		return $this->getApiResponse($request,self::RESPONSE_XML);
 	}
-	
+
 	/**
 	 * Fetch all ship routes as json
 	 * @Route("/routes.csv",name="getRoutesAsCsv")
 	 * @Method("GET")
 	 */
 	public function getVeselRoutesCsv(Request $request)
-	{	
+	{
 		return $this->getApiResponse($request,self::RESPONSE_CSV);
 	}
-	
+
 	/**
 	 * I developed this method because the code is similar and only a very small ammount of it changes.
-	 * 
+	 *
 	 * @param Request $request
 	 * @param unknown $whatToSerialize
 	 * @return \Symfony\Component\HttpFoundation\Response
-	 * 
+	 *
 	 */
 	private function getApiResponse(Request $request,$whatToSerialize)
 	{
 		$response=new Response();
 		$response->headers->set('Content-type',$whatToSerialize);
-		
+
 		$whatToSerialize=explode('/',$whatToSerialize);
 		$whatToSerialize=end($whatToSerialize);
 		$response->setContent($whatToSerialize);
-		
+
 		//The return result
 		$data=null;
-		
+
 		try {
 			$data=$this->getVeselRoutesFromDb($request);
 			if($whatToSerialize==='csv')//Only CSV will be displayed differently
-			{				
+			{
 				$data=$this->get('twig')->render('routes/routes.csv.twig',['vesels'=>$data]);
 			} else {
 				$serializer=$this->get('jms_serializer');
-				$data=$serializer->serialize($data, $whatToSerialize);	
+				$data=$serializer->serialize($data, $whatToSerialize);
 			}
 		} catch(EmptyParamGivenException $ep) {
 			throw new ApiEndpointException($ep->getMessage(),$response->headers->all(),Response::HTTP_BAD_REQUEST,$whatToSerialize);
 		} catch(\Exception $e) {
 			throw new ApiEndpointException($e->getMessage(),$response->headers->all(),Response::HTTP_INTERNAL_SERVER_ERROR,$whatToSerialize);
 		}
-		
+
 		$response->setContent($data);
 		return $response;
 	}
-	
-	
+
+
 	/**
 	 * Calls the repository (that has the role of the mvc MODEL) and returns the data.
-	 * 
+	 *
 	 * I seperated the model call with the other route functions
 	 * because I wanted to have more clean code and reusable one.
-	 * 
+	 *
 	 * @return void
 	 * @throws EmptyParamGivenException
 	 * @throws Exception
-	 * 
+	 *
 	 * @return Vesel[]
 	 */
 	private function getVeselRoutesFromDb(Request $request)
 	{
-		$veselMMSID=$request->get('mmsid');
-		
-		if(!is_string($veselMMSID)){
+		$veselMMSID=$request->get('mmsi');
+
+		if(is_string($veselMMSID)){
 			$veselMMSID=explode(";",$veselMMSID);
 		}
-		
+
 		$latitudeMin=$request->get('latitude_min');
 		$latitudeMax=$request->get('latitude_max');
 		$longtitudeMin=$request->get('longtitude_min');
 		$longtitudeMax=$request->get('longtitude_max');
-		
+
 		$repository=$this->get('vesel_repository');
-		
+
 		$data=$repository->getRoutes($veselMMSID,$latitudeMin,$latitudeMax,$longtitudeMin,$longtitudeMax);
 		return $data;
 	}
--- dimitriosDesyllas/src/AppBundle/Repository/VeselRouteRepository.php
+++ dimitriosDesyllas/src/AppBundle/Repository/VeselRouteRepository.php
@@ -16,10 +16,10 @@ class VeselRouteRepository extends EntityRepository
 	 * @param unknown $latitudeMin
 	 * @param unknown $latitudeMax
 	 * @param \Datetime $timeInterval
-	 * 
+	 *
 	 * @throws EmptyParamGivenException
 	 * @throws Exception
-	 * 
+	 *
 	 * @return Vesel[] with their routes
 	 */
 	public function getRoutes(array $mmsids=[],
@@ -30,26 +30,25 @@ class VeselRouteRepository extends EntityRepository
 									\Datetime $timeInterval=null
 	) {
 		$em=$this->getEntityManager();
-		
+
 		$query=$em->createQueryBuilder('v')
 				->from('AppBundle:Vesel', 'v')
 				->innerJoin('v.veselMoveStatuses','m')
 				->select('v')
-				//->orderBy('v.mmsi','ASC')
+				->orderBy('v.mmsi','ASC')
 				->orderBy('m.timestamp','ASC');
-		
-		
-// 		if(InputValidator::validateCoordinatesRange($longituteMin,$longtitudeMax,$latitudeMin,$latitudeMax)){
-// 			$query->where("m.long BETWEEN :long_min AND :long_max")
-// 				->andWhere("m.lat BETWEEN :lat_min AND :lat_max")
-// 				->setParameters(['long_min'=>$longituteMin,'long_max'=>$longtitudeMax,'lat_min'=>$latitudeMin,'lat_max'=>$latitudeMax]);
-// 		}
-		
-// 		if(!empty($mmsids)){
-// 			var_dump($mmsids);
-// 			$query->andWhere('v.mmsi IN (:mmsids)')->setParameter('mmsids', $mmsids,\Doctrine\DBAL\Connection::PARAM_STR_ARRAY);
-// 		}
-		
+
+
+		// if(InputValidator::validateCoordinatesRange($longituteMin,$longtitudeMax,$latitudeMin,$latitudeMax)){
+		// 	$query->where("m.long BETWEEN :long_min AND :long_max")
+		// 		->andWhere("m.lat BETWEEN :lat_min AND :lat_max")
+		// 		->setParameters(['long_min'=>$longituteMin,'long_max'=>$longtitudeMax,'lat_min'=>$latitudeMin,'lat_max'=>$latitudeMax]);
+		// }
+		//
+		if(!empty($mmsids)){
+			$query->andWhere('v.mmsi IN (:mmsids)')->setParameter('mmsids', $mmsids,\Doctrine\DBAL\Connection::PARAM_STR_ARRAY);
+		}
+
 		$query = $query->getQuery();
 		return $query->getResult();
 	}
