f1f0803113e391c07cc771da53f2a5595e98f325
Christos Mantzouranis <mantzouranisc@gmail.com>
2017-10-24 14:53:39 +0300

    Readme and cleanup (#2)
    * removed candidate folder
    
    * Updated readme file

--- dimitriosDesyllas/src/AppBundle/Exception/InvalidRangeException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace AppBundle\Exception;
-
-class InvalidRangeException extends \Exception{
-	public function __construct($paramNameThatShouldBeBigger,$paramNameThatShouldBeSmaller){
-		parent::__construct("Param $paramNameThatShouldBeBigger must be bigger than $paramNameThatShouldBeSmaller");
-	}
-}
